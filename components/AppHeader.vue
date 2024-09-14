2ee3ab4980f3da02c3e07532dd6efcd919c710c8
NimeshaKahingala <nimesha.isholi94@gmail.com>
2024-09-14 12:39:46 -0500

    basic pages added

---
+++ components/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/product">Product</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+  
---
+++ pages/product.vue
@@ -0,0 +1,6 @@
+<template>
+    <section>
+        <h1>Product</h1>
+      <p>This page will be displayed at the /product route.</p>
+    </section>
+  </template>
