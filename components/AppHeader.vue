0faf4db94eb47189f8990b29efb63af4681e737d
NimeshaKahingala <nimesha.isholi94@gmail.com>
2024-09-15 14:22:31 -0500

    product cards added

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,12 +1,25 @@
 <template>
     <header>
       <nav>
-        <ul>
-          <li><NuxtLink to="/">Home</NuxtLink></li>
-          <li><NuxtLink to="/about">About</NuxtLink></li>
-          <li><NuxtLink to="/product">Product</NuxtLink></li>
+        <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+        <ul class="flex">
+          <DisclosureButton  class="text-white [item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"><NuxtLink to="/">Home</NuxtLink></DisclosureButton>
+          <li class="text-white [item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"><NuxtLink to="/about">About</NuxtLink></li>
+          <li class="text-white [item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"><NuxtLink to="/product">Product</NuxtLink></li>
         </ul>
+        </Disclosure>
       </nav>
     </header>
   </template>
   
+  <script setup>
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+//import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+
+// const navigation = [
+//   { name: 'Dashboard', href: '#', current: true },
+//   { name: 'Team', href: '#', current: false },
+//   { name: 'Projects', href: '#', current: false },
+//   { name: 'Calendar', href: '#', current: false },
+// ]
+</script>
--- pages/product.vue
+++ pages/product.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-        <h1>Product</h1>
-      <p>This page will be displayed at the /product route.</p>
+        <h1>HELPFUL TAGLINE/ DESCRIPTION</h1>
+      <ProductCard/>
     </section>
   </template>
