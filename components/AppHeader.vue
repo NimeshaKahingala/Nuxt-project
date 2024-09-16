61d15fb39b0321f86bad8ba9b021c29c4aa3e70f
NimeshaKahingala <nimesha.isholi94@gmail.com>
2024-09-15 19:33:04 -0500

    nav bar content updated'

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,9 +3,9 @@
       <nav>
         <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
         <ul class="flex">
-          <DisclosureButton  class="text-white [item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"><NuxtLink to="/">Home</NuxtLink></DisclosureButton>
+          <DisclosureButton  class="text-white [item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"><NuxtLink to="/">HELPFUL</NuxtLink></DisclosureButton>
           <li class="text-white [item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"><NuxtLink to="/about">About</NuxtLink></li>
-          <li class="text-white [item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"><NuxtLink to="/product">Product</NuxtLink></li>
+          <li class="text-white [item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"><NuxtLink to="/products">Product</NuxtLink></li>
         </ul>
         </Disclosure>
       </nav>
