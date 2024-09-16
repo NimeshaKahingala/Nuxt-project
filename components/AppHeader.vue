2be088b587a9e117037b45e25745af400a162cf0
NimeshaKahingala <nimesha.isholi94@gmail.com>
2024-09-15 22:21:17 -0500

    nav bar content updated'

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,9 +3,10 @@
       <nav>
         <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
         <ul class="flex">
-          <DisclosureButton  class="text-white [item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"><NuxtLink to="/">HELPFUL</NuxtLink></DisclosureButton>
-          <li class="text-white [item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"><NuxtLink to="/about">About</NuxtLink></li>
-          <li class="text-white [item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"><NuxtLink to="/products">Product</NuxtLink></li>
+          <DisclosureButton  class="text-white uppercase [item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"><NuxtLink to="/">HELPFUL</NuxtLink></DisclosureButton>
+          <li class="text-white uppercase [item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"><NuxtLink to="/about">About</NuxtLink></li>
+          <li class="text-white uppercase [item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"><NuxtLink to="/contact">Contact</NuxtLink></li>
+          <li class="text-white uppercase [item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"><NuxtLink to="/signin">Sign IN</NuxtLink></li>
         </ul>
         </Disclosure>
       </nav>
