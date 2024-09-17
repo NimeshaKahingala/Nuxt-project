1be985e2d547b81ae9f07acd3bb171ac37986a43
NimeshaKahingala <nimesha.isholi94@gmail.com>
2024-09-17 02:05:27 -0500

    register page added'

---
+++ pages/register.vue
@@ -0,0 +1,7 @@
+<template>
+    <section>
+        <h1 class="text-2xl font-bold tracking-tight text-red-900">Register</h1>
+      <p>This page will be displayed at the /register route.</p>
+    </section>
+  </template>
+  
